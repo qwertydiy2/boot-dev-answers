412efc5579b62c88000e0fab572a5c6a586d329a
qwertydiy2 <107488390+qwertydiy2@users.noreply.github.com>
2025-04-18 12:00:49 +0100

    feat(structs): add exercise 5 for embedded structs in Go

---
+++ go/courses/learn-go/chapter-4-structs/exercise-5-embedded-structs/main.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+type sender struct {
+	user
+	rateLimit int
+}
+
+type user struct {
+	name   string
+	number int
+}
+
+func getSenderLog(s sender) string {
+	return fmt.Sprintf(`
+====================================
+Sender name: %v
+Sender number: %v
+Sender rateLimit: %v
+====================================
+`, s.name, s.number, s.rateLimit)
+}
+
+func main() {
+	// Run tests
+	runTests()
+}
+
+func runTests() {
+	fmt.Println("\nRunning tests...\n")
+
+	// Create a test suite
+	var t testing.T
+
+	// Run the test cases
+	Test(&t)
+
+	if t.Failed() {
+		fmt.Println("\nSome tests failed. See output above.")
+	} else {
+		fmt.Println("\nAll tests passed!")
+	}
+}
+
+func Test(t *testing.T) {
+	type testCase struct {
+		rateLimit int
+		name      string
+		number    int
+		expected  string
+	}
+
+	runCases := []testCase{
+		{
+			10000,
+			"Deborah",
+			18055558790,
+			`
+====================================
+Sender name: Deborah
+Sender number: 18055558790
+Sender rateLimit: 10000
+====================================
+`,
+		},
+		{
+			5000,
+			"Jason",
+			18055558791,
+			`
+====================================
+Sender name: Jason
+Sender number: 18055558791
+Sender rateLimit: 5000
+====================================
+`,
+		},
+	}
+	submitCases := append(runCases, []testCase{
+		{
+			1000,
+			"Jill",
+			18055558792,
+			`
+====================================
+Sender name: Jill
+Sender number: 18055558792
+Sender rateLimit: 1000
+====================================
+`,
+		},
+	}...)
+
+	testCases := runCases
+	if withSubmit {
+		testCases = submitCases
+	}
+	skipped := len(submitCases) - len(testCases)
+
+	passCount := 0
+	failCount := 0
+
+	for _, test := range testCases {
+		output := getSenderLog(sender{
+			rateLimit: test.rateLimit,
+			user: user{
+				name:   test.name,
+				number: test.number,
+			},
+		})
+		if output != test.expected {
+			failCount++
+			t.Errorf(`---------------------------------
+Inputs:     (%v, %v, %v)
+Expecting:  %v
+Actual:     %v
+Fail
+`, test.rateLimit, test.name, test.number, test.expected, output)
+		} else {
+			passCount++
+			fmt.Printf(`---------------------------------
+Inputs:     (%v, %v, %v)
+Expecting:  %v
+Actual:     %v
+Pass
+`, test.rateLimit, test.name, test.number, test.expected, output)
+		}
+	}
+
+	fmt.Println("---------------------------------")
+	if skipped > 0 {
+		fmt.Printf("%d passed, %d failed, %d skipped\n", passCount, failCount, skipped)
+	} else {
+		fmt.Printf("%d passed, %d failed\n", passCount, failCount)
+	}
+}
+
+// withSubmit is set at compile time depending on which button is used to run the tests
+var withSubmit = true
