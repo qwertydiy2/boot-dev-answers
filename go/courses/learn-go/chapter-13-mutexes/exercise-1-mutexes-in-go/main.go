ed9a02bb979417d1c7bf1adb2b5ba39b4df9a811
qwertydiy2 <107488390+qwertydiy2@users.noreply.github.com>
2025-10-17 10:46:06 +0100

    Implement safeCounter with mutex for concurrency

---
+++ go/courses/learn-go/chapter-13-mutexes/exercise-1-mutexes-in-go/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type safeCounter struct {
+	counts map[string]int
+	mu     *sync.Mutex
+}
+
+func (sc safeCounter) inc(key string) {
+	// ?
+	sc.mu.Lock()
+	sc.slowIncrement(key)
+	sc.mu.Unlock()
+}
+
+func (sc safeCounter) val(key string) int {
+	// ?
+	return sc.slowVal(key)
+}
+
+// don't touch below this line
+
+func (sc safeCounter) slowIncrement(key string) {
+	tempCounter := sc.counts[key]
+	time.Sleep(time.Microsecond)
+	tempCounter++
+	sc.counts[key] = tempCounter
+}
+
+func (sc safeCounter) slowVal(key string) int {
+	time.Sleep(time.Microsecond)
+	return sc.counts[key]
+}
+
+func main() {
+	type testCase struct {
+		email string
+		count int
+	}
+
+	runCases := []testCase{
+		{"norman@bates.com", 23},
+		{"marion@bates.com", 67},
+	}
+
+	submitCases := append(runCases, []testCase{
+		{"lila@bates.com", 31},
+		{"sam@bates.com", 453},
+	}...)
+
+	testCases := runCases
+	if withSubmit {
+		testCases = submitCases
+	}
+
+	skipped := len(submitCases) - len(testCases)
+
+	passCount := 0
+	failCount := 0
+
+	for _, test := range testCases {
+		sc := safeCounter{
+			counts: make(map[string]int),
+			mu:     &sync.Mutex{},
+		}
+		var wg sync.WaitGroup
+		for i := 0; i < test.count; i++ {
+			wg.Add(1)
+			go func(email string) {
+				sc.inc(email)
+				wg.Done()
+			}(test.email)
+		}
+		wg.Wait()
+
+		if output := sc.val(test.email); output != test.count {
+			failCount++
+			fmt.Printf(`---------------------------------
+Test Failed:
+  email: %v
+  count: %v
+  expected count: %v
+  actual count:   %v
+`, test.email, test.count, test.count, output)
+		} else {
+			passCount++
+			fmt.Printf(`---------------------------------
+Test Passed:
+  email: %v
+  count: %v
+  expected count: %v
+  actual count:   %v
+`, test.email, test.count, test.count, output)
+		}
+	}
+
+	fmt.Println("---------------------------------")
+	if skipped > 0 {
+		fmt.Printf("%d passed, %d failed, %d skipped\n", passCount, failCount, skipped)
+	} else {
+		fmt.Printf("%d passed, %d failed\n", passCount, failCount)
+	}
+}
+
+// withSubmit is set at compile time depending
+// on which button is used to run the tests
+var withSubmit = true
